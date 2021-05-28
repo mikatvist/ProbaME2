138747742c4c1729e23130a7b49c31a8bd7a940e
mikatvist <mikatvist@gmail.com>
2021-05-28 11:52:34 -0400

    reverse1

---
+++ src/test/java/ReverseAString.java
@@ -0,0 +1,11 @@
+public class ReverseAString {
+    public static void main(String[] args) {
+        int a = 1;
+        int q = 2;
+        if (a==q){
+            System.out.println("proba");
+        }else {
+            System.out.println("kiss");
+        }
+    }
+}
