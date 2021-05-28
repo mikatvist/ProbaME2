79234b0b378996270883a6331ce7f5be46ab1220
mikatvist <69088570+mikatvist@users.noreply.github.com>
2021-05-27 22:21:37 -0400

    Update ProbaME2.java

--- src/test/java/ProbaME2.java
+++ src/test/java/ProbaME2.java
@@ -2,6 +2,8 @@ public class ProbaME2 {
     public static void main(String[] args) {
         System.out.println("Mne ploho");
         
+        //"commit changes"
+        
         // i add something
     }
 }
